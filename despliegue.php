8434f9c0ef5918bde88a3ee159c33298a5de4666
karolll8 <128723904+karolll8@users.noreply.github.com>
2023-10-20 12:23:05 -0500

    Add files via upload

--- despliegue.php
+++ despliegue.php
@@ -4,7 +4,7 @@
 include("funciones.php");//se incluye el archivo funciones.
 
 
-echo consultar(null, null, false, 5); //se invoca la funcion consulta
+echo consultar(null, null, false, 'Nombre_usuario',4 ); //se invoca la funcion consulta
 
 echo "<br>"; //se hace espacio
 
--- funciones.php
+++ funciones.php
@@ -6,55 +6,78 @@
  * @param   $c   texto   varchar not null
  * @param   $e   texto   varchar null
  * @param   $l   numero  varchar null
+ * @param   $f  texto   varchar null
  * @return  texto
  *  @mysqli_stmt_bind_param: usados para asociar los valores que se quieren
  *  insertar o consultar en la base de datos con los marcadores de posición
  *  en la consulta SQL.
  */
-function consultar($u = null, $c = null, $e = false,$l=null)
+function consultar($u = null, $c = null, $e = false, $f = null, $l = null)
 {
-     $salida = ""; // Inicializa la variable
-    $conexion = mysqli_connect("localhost", "root", "root", "RUBLE_FORGOTAPP_PROYECT"); // Conexión con la base de datos
-    $sql = "SELECT * FROM Usuarios LIMIT $l"; // Consulta SQL
+    $salida = ""; // Inicializa la variable
+    $conexion = new mysqli("localhost", "root", "root", "RUBLE_FORGOTAPP_PROYECT"); // Conexión con la base de datos
+
+    // Consulta SQL
+    $sql = "SELECT * FROM Usuarios";
+    if ($f !== null) {
+        // Si se especifica un campo específico, cambiamos la consulta para seleccionar solo ese campo
+        $sql = "SELECT $f FROM Usuarios";
+    }
+
     if ($u !== null && $c !== null) { // Filtrar por ID y Contraseña
         $sql .= " WHERE Id = ? AND Contraseña = ?";
     } elseif ($u !== null) { // Filtrar solo por ID
         $sql .= " WHERE Id = ?";
     }
-    $stmt = mysqli_prepare($conexion, $sql); // Preparar consulta SQL parametrizada
-    if ($u !== null && $c !== null) { // Si se especifican tanto $u como $c
-        mysqli_stmt_bind_param($stmt, "ss", $u, $c);
-    } elseif ($u !== null) { // Si solo se especifica $u
-        mysqli_stmt_bind_param($stmt, "s", $u);
+
+    if ($l !== null) {
+        $sql .= " LIMIT " . intval($l);
     }
+
+    $stmt = $conexion->prepare($sql); // Preparar consulta SQL parametrizada
+
+    if ($u !== null && $c !== null) { // Si se especifican tanto $u como $c
+        $stmt->bind_param("ss", $u, $c);
+    } elseif ($u !== null) { // Si solo se especifica $u
+        $stmt->bind_param("s", $u);
+    }
+
     if ($e) { // Si se solicita el conteo de usuarios
-        $sql = "SELECT COUNT(*) AS total FROM Usuarios"; // Consulta para contar todos los usuarios
-        $stmt = mysqli_prepare($conexion, $sql);//preparar la consulta
-        mysqli_stmt_execute($stmt);//ejecutar consulta
-        $resultadoCount = mysqli_stmt_get_result($stmt);//obtener los datos de consulta
-        $filaCount = mysqli_fetch_assoc($resultadoCount);//incorporarlo como fetch_assoc
+        $sqlCount = "SELECT COUNT(*) AS total FROM Usuarios"; // Consulta para contar todos los usuarios
+        $stmt = $conexion->prepare($sqlCount);
+        $stmt->execute();
+        $resultadoCount = $stmt->get_result();
+        $filaCount = $resultadoCount->fetch_assoc();
         $salida = "Total de usuarios: " . $filaCount['total'];
     } else { // Mostrar los resultados
-        if (mysqli_stmt_execute($stmt)) { // Verificar si la consulta fue exitosa
-            $resultado = mysqli_stmt_get_result($stmt); // Obtener el resultado
+        if ($stmt->execute()) { // Verificar si la consulta fue exitosa
+            $resultado = $stmt->get_result(); // Obtener el resultado
             if ($resultado->num_rows > 0) { // Si hay registros
                 while ($fila = $resultado->fetch_assoc()) {
-                    $salida .= "<td>" . $fila['Id'] . "</td>";
-                    $salida .= "<td>" . $fila['Nombre_usuario'] . "</td>";
-                    $salida .= "<td>" . $fila['Correo'] . "</td>";
-                    $salida .= "<td>" . $fila['Contraseña'] . "</td>";
-                    $salida .= "<td>" . $fila['Cumpleaños'] . "</td>";
-                    $salida .= "<td>" . $fila['Telefono'] . "</td>";
-                    $salida .= "<td>" . $fila['N°'] . "</td><br>";
+                    // Verificar si se especificó un campo específico
+                    if ($f !== null) {
+                        $salida .= "<td>" . $fila[$f] . "</td>";
+                    } else {
+                        // Si no se especifica un campo específico, mostrar todos los campos
+                        $salida .= "<tr>";
+                        $salida .= "<td>" . $fila['Id'] . "</td>";
+                        $salida .= "<td>" . $fila['Nombre_usuario'] . "</td>";
+                        $salida .= "<td>" . $fila['Correo'] . "</td>";
+                        $salida .= "<td>" . $fila['Contraseña'] . "</td>";
+                        $salida .= "<td>" . $fila['Cumpleaños'] . "</td>";
+                        $salida .= "<td>" . $fila['Telefono'] . "</td>";
+                        $salida .= "<td>" . $fila['N°'] . "</td><br>";
+                        $salida .= "</tr>";
+                    }
                 }
             } else {
                 $salida = "No se encontraron registros con los filtros especificados.";
             }
         } else {
-            $salida = "Error en la consulta: " . mysqli_error($conexion);
+            $salida = "Error en la consulta: " . $stmt->error;
         }
     }
-    mysqli_stmt_close($stmt); // Cerrar consulta
+    $stmt->close(); // Cerrar consulta
     $conexion->close(); // Cerrar la conexión
     return $salida; // Retornar el resultado
 }
